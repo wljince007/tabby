caab9730c945e0b4a2990be6a743ecdd14dc7610
liangwang <liangwang007@gmail.com>
2022-12-04 18:40:08 +0800

    sftp脚本每个平台的模板不同,以配置来进行,目前没有找到方法做到自动在生成配置文件时添加,配雇文件为空时自动增加sftp的模板的profiles

--- tabby-ssh/src/config.ts
+++ tabby-ssh/src/config.ts
@@ -3,54 +3,6 @@ import { ConfigProvider } from 'tabby-core'
 /** @hidden */
 export class SSHConfigProvider extends ConfigProvider {
     defaults = {
-        profiles: [
-            {
-                "type":"local",
-                "name":"ssh2sftp_win_template",
-                "icon":"fas fa-terminal",
-                "options":{
-                    "command":"/usr/bin/sftp",
-                    "env":{
-        
-                    },
-                    "cwd":""
-                },
-                "group":"ssh2sftp_template",
-                "id":"local:custom:ssh2sftp_win_template:c617da05-d05c-482d-8ca6-3c7eb99452e9"
-            },
-            {
-                "type":"local",
-                "name":"ssh2sftp_linux_template",
-                "icon":"fas fa-terminal",
-                "options":{
-                    "command":"/usr/bin/sftp",
-                    "env":{
-        
-                    },
-                    "cwd":""
-                },
-                "group":"ssh2sftp_template",
-                "id":"local:custom:ssh2sftp_linux_template:6c4bcc75-f690-482a-a882-40e1c9851a3d"
-            },
-            {
-                "type":"local",
-                "name":"ssh2sftp_mac_template",
-                "icon":"fas fa-terminal",
-                "options":{
-                    "command":"/usr/local/opt/openssh/bin/sftp",
-                    "args":[
-                        "-o",
-                        "StrictHostKeyChecking=no"
-                    ],
-                    "env":{
-        
-                    },
-                    "cwd":""
-                },
-                "id":"local:custom:ssh2sftp_mac_template:33162a26-7807-4c5e-ac2d-68cd2d9a4a24",
-                "group":"ssh2sftp_template"
-            }
-        ],
         ssh: {
             warnOnClose: false,
             winSCPPath: null,
